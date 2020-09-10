edc1289773c64dd339bd4bc722d8712f769a4a98
Kay Warrie <kaywarrie@gmail.com>
2020-09-10 15:40:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Stad in Cijfers, python API
+================
+
+Purpose
+----
+
+A python package to make data from stadincijfers easely available for data scientists. 
+
+Intro
+-----
+
+Via the website 'Stad in cijfers' you can consult statistical data on various themes. In principle for the city of Antwerp, all data on 'Stad in cijfers' is open data and can therefore be used freely, unless stated otherwise.
+
+Many local goverments in Flanders have a website like this:
+
+- Gent: https://gent.buurtmonitor.be
+- Antwerp: https://stadincijfers.antwerpen.be/databank
+- The provinces: https://provincies.incijfers.be
+
+Installation:
+--------------
+
+    pip install stadincijfers
+
+
+Usage:
+-----
+
+See example jupyter notebook: [example_usage.ipynb](example_usage.ipynb)
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stadincijfers.py", 
+    version="0.1.0",
+    author="Kay Warrie",
+    author_email="kaywarrie@gmail.com",
+    description="A python package to make data from stadincijfers easely available for data scientists. ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/warrieka/stadincijfers.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[ 'pandas' ],
+    python_requires='>=2.7',
+)
