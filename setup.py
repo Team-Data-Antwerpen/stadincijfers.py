1d2d43edbf0db3eda7745124a80865b5ba0dbe77
Sofie Cromheeke <sofie.cromheeke@gmail.com>
2025-07-30 10:40:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stadincijfers", 
-    version="0.2.2",
+    version="0.2.3",
     author="Kay Warrie, Sofie Cromheeke",
     author_email="kaywarrie@gmail.com, sofie.cromheeke@gmail.com",
     description="A python package to make data from stadincijfers easily available for data scientists. ",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/warrieka/stadincijfers.py",
+    url="https://github.com/Team-Data-Antwerpen/stadincijfers.py",
     packages=["stadincijfers"],
     classifiers=[
         "Programming Language :: Python :: 3",
